549109188fffd8d30754005e876db60394f2d53e
James Quillen <james.quillen@gmail.com>
2023-03-09 11:00:13 -0800

    final commit before migration to another repo

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,3 +1,15 @@
 class Animal < ApplicationRecord
     has_many :animal_sightings
+    validates :common_name, :scientific_binomial, presence: true
+    validate :unique_names
+    validates :common_name, uniqueness: true
+    validates :scientific_binomial, uniqueness: true
+
+    def unique_names
+        if common_name == scientific_binomial
+            errors.add( :common_name, 'Common name cannot be the same a scientific binomial')
+            errors.add( :scientific_binomial, 'Common name cannot be the same a scientific binomial')
+        end
+    end
+
 end
